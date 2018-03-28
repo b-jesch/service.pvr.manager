39e8289b2c30d9d156d7937fb837ab4c720383d1
b-jesch <birger.jesch@gmail.com>
2018-03-28 12:55:12 +0200

    Bugfix: Handle empty values in os-release on some platforms #2

--- resources/lib/tools.py
+++ resources/lib/tools.py
@@ -48,11 +48,9 @@ def release():
     if coll['platform'] == 'Linux':
         with open('/etc/os-release', 'r') as _file:
             for _line in _file:
-                try:
+                if '=' in _line:
                     parameter, value = _line.split('=')
                     item[parameter] = value.replace('"', '').strip()
-                except ValueError:
-                    item[parameter] = 'n.a.'
 
     coll.update({'osname': item.get('NAME', ''), 'osid': item.get('ID', ''), 'osversion': item.get('VERSION_ID', '')})
     return coll
